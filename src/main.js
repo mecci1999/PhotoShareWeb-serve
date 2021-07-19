38a379a413bed6fd8e78458b0667426bf09dfb0e
cc <664751829@qq.com>
2021-07-19 21:05:44 +0800

    创建 Web 服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
-const greeting = '您好,Github!';
-console.log(greeting);
+const http = require('http');
+
+const server = http.createServer((request,response) => {
+  response.write('hello~');
+  response.end();       //响应结束
+});
+
+//服务器的监听
+server.listen(3000,() => {
+  console.log('服务器已经启动！');
+});
