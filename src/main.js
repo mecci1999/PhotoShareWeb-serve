3d4e53141d8aa973e6f764c1e864475f39bbc5b1
cc <664751829@qq.com>
2021-07-20 17:26:40 +0800

    使用 JSON 中间件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;    //端口号
 
+/**
+ * 使用 JSON 中间件
+ */
+app.use(express.json());
+
 app.listen(port, () => {
   console.log('服务器已启动！');
 });
