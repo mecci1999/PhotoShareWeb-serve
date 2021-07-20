f04f15aede640312855efa764476fe3706286b28
cc <664751829@qq.com>
2021-07-20 15:09:08 +0800

    使用Express创建服务器

--- src/main.js
+++ src/main.js
@@ -1,11 +1,42 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;    //端口号
 
-const server = http.createServer((request,response) => {
-  response.write('hello~');
-  response.end();       //响应结束
+app.listen(port, () => {
+  console.log('服务器已启动！');
 });
 
-//服务器的监听
-server.listen(3000,() => {
-  console.log('服务器已经启动！');
+app.get('/', (request,response) => {
+  response.send('你好！');
+});
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间'
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小'
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红似火，春来江水绿如蓝'
+  }
+];
+
+app.get('/posts', (request,response) => {
+  response.send(data);
+});
+
+app.get('/posts/:postId', (request,response) => {
+  //获取Id
+  const { postId } = request.params;
+
+  //查找具体内容
+  const posts = data.filter(item => item.id == postId);
+  response.send(posts[0]);
 });
