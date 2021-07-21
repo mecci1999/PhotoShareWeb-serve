64c43302f98e353f29fdf2aa57dcf7e5c3e6002b
cc <664751829@qq.com>
2021-07-21 21:51:44 +0800

    组件：默认异常处理器的创建

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import postRouter from '../post/post.router';
+import { defaultErrorHandler } from './app.middleware';
 /**
  * 创建应用
  */
@@ -14,6 +15,11 @@ app.use(express.json());
  *路由 
  */
 app.use(postRouter);
+
+/**
+ * 使用异常处理器
+ */
+app.use(defaultErrorHandler);
 
 /**
  * 最后默认导出应用
