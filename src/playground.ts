65d72ffc88c67677091a5b6f5814dafaaa7db7ef
cc <664751829@qq.com>
2021-07-22 16:03:38 +0800

    异步函数async和await的练习

--- src/playground.ts
+++ src/playground.ts
@@ -9,9 +9,11 @@ const nature = () => {
   });
 };
 
-
-nature().then(data => {
+const demo = async () => {
+  const data = await nature();
   console.log(data);
-})
+};
+
+demo();
 
 console.log('火山');
