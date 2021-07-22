b93eb83f235c557a3c3343e79cf8a7f27473b9bd
cc <664751829@qq.com>
2021-07-22 22:09:15 +0800

    利用数据之间的关系获取更详细的内容数据

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -6,7 +6,19 @@ export const getPosts = async () => {
   /**
    * 数据仓库连接
    */ 
-  const statement = `SELECT * FROM post`;
+  const statement = `
+  SELECT 
+    post.id,
+    post.title,
+    post.content,
+    JSON_OBJECT(
+      'id', user.id,
+      'name', user.name
+    ) as user
+  FROM post
+  LEFT JOIN user
+    ON user.id = post.userId
+  `;
   const [data] = await connection.promise().query(statement);
   return data;
 };
