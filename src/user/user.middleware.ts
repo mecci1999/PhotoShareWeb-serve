6f9cf30459c22e8f762af123c6bebb08d4c55398
cc <664751829@qq.com>
2021-07-23 16:59:43 +0800

    验证用户数据

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,6 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from './user.service';
 
+
 /**
- * 默认异常处理器
+ * 验证用户数据
  */
+export const validataUserData = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  console.log("验证用户数据中~~~");
+
+  //准备数据
+  const {name, password} = request.body;
+
+  //验证必填信息
+  if(!name) return next(new Error('NAME_IS_REQUIRED'));
+  if(!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+
+  //下一步操作
+  next();
+};
