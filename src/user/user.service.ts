d1ea0bfd65d5c62367d1829bec24a7a24e753ecb
cc <664751829@qq.com>
2021-07-24 10:23:35 +0800

    定义按用户名查找用户的服务

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,3 +17,21 @@ export const createUser = async (user: UserModel) => {
   //提供数据
   return data;
 };
+
+/**
+ * 按用户名查找用户
+ */
+export const getuserName = async (name: string) => {
+  //准备查询
+  const statement = `
+    SELECT id,name
+    FROM user
+    WHERE name = ?
+  `;
+  
+  //执行查询
+  const [data,...data2] = await connection.promise().query(statement,name);
+
+  //提供数据
+  return data;
+};
