718f311208d815c64b7d21896ac3ef47d839c4a8
cc <664751829@qq.com>
2021-07-26 16:17:09 +0800

    定义检查用户是否拥有指定资源的功能

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { PRIVATE_KEY } from '../app/app.config';
+import { connection } from '../app/database/mysql';
 
 /**
  * 签发信息
@@ -20,4 +21,31 @@ export const signToken = (options: SignTokenOptions) => {
   
   //提供数据
   return token;
+};
+
+/**
+ * 检查用户是否拥有指定资源
+ */
+interface ProssessOptions {
+  resourceId: number;
+  resourceType: string;
+  userId?: number;
+}
+
+export const prossess = async (options: ProssessOptions) => {
+  // 准备选项
+  const { resourceId, resourceType, userId} = options;
+  
+  // 准备查询
+  const statement =  `
+    SELECT COUNT(${resourceType}.id) as count
+    FROM ${resourceType}
+    WHERE ${resourceType}.id = ? AND userId = ?
+  `;
+
+  // 执行查询
+  const [...data] = await connection.promise().query(statement, [resourceId, userId]);
+
+  // 提供数据
+  return data[0].count ? true : false;
 };
