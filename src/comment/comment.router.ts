fe291e04ecd67d78ca032a6d391609372492f136
cc <664751829@qq.com>
2021-07-29 15:21:38 +0800

    定义修改内容的接口

--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as commentMiddleware from './comment.middleware';
 import * as commentController from './comment.controller';
-import { authGuard } from '../auth/auth.middleware';
+import { accessControl, authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
 
@@ -14,6 +14,11 @@ router.post('/comments', authGuard, commentController.store);
  * 回复评论
  */
 router.post('/comments/:commentId/reply', authGuard, commentController.reply);
+
+/**
+ * 修改评论
+ */
+router.patch('/comments/:commentId', authGuard, accessControl({prossession: true}), commentController.update);
 
 /**
  * 导出默认接口
