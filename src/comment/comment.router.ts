a8f6405daba1e30034f752151b9b6caeadfd9609
cc <664751829@qq.com>
2021-07-29 10:57:17 +0800

    定义发表评论的接口

--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import * as commentMiddleware from './comment.middleware';
 import * as commentController from './comment.controller';
+import { authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
+
+/**
+ * 发表评论
+ */
+router.post('/comments', authGuard, commentController.store);
 
 /**
  * 导出默认接口
