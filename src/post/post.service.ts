fa255873f202025509d84e4d027b75c469018cbf
cc <664751829@qq.com>
2021-07-30 14:27:50 +0800

    查询片段

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,5 +1,6 @@
 import { connection } from '../app/database/mysql';
 import { PostModel } from './post.model';
+import { sqlFragment } from './post.provider';
 /**
  * 定义一个函数：获取内容列表
  */
@@ -13,13 +14,9 @@ export const getPosts = async () => {
     post.id,
     post.title,
     post.content,
-    JSON_OBJECT(
-      'id', user.id,
-      'name', user.name
-    ) as user
+    ${sqlFragment.user}
   FROM post
-  LEFT JOIN user
-    ON user.id = post.userId
+  ${sqlFragment.leftJoinUser}
   `;
   const [data] = await connection.promise().query(statement);
   return data;
