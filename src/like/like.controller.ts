d8b082e8b8e66ba81848dfa4f4c8aa240f0935bd
cc <664751829@qq.com>
2021-07-31 17:28:14 +0800

    定义用户点赞内容接口

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,1 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
+import { createUserLikePost } from './like.service';
+
+/**
+* 点赞内容
+*/
+export const storeUserLikePosr = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  //获得数据
+  const {postId} = request.params;
+  const {id: userId} = request.user;
+
+  //保存点赞
+  const data = await createUserLikePost(parseInt(`${userId}`, 10), parseInt(postId, 10));
+
+  //做出响应
+  response.status(201).send(data);
+};
--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import * as likeMiddleware from './like.middleware';
 import * as likeController from './like.controller';
+import { authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
+
+/**
+ * 用户点赞内容
+ */
+router.post('/posts/:postId/like', authGuard, likeController.storeUserLikePosr);
 
 /**
  * 导出默认接口
