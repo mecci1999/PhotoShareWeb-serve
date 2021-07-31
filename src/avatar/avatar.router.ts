98b56124b9ace05877fc3dd150e9dcf9d6ca5ec4
cc <664751829@qq.com>
2021-07-31 10:44:40 +0800

    定义上传头像的接口

--- src/avatar/avatar.router.ts
+++ src/avatar/avatar.router.ts
@@ -1,8 +1,14 @@
 import express, { Router } from 'express';
 import * as avatarMiddleware from './avatar.middleware';
 import * as avatarController from './avatar.controller';
+import { authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
+
+/**
+ * 上传头像文件
+ */
+router.post('/avatar', authGuard, avatarMiddleware.avatarInterceptor, avatarController.store);
 
 /**
  * 默认导出接口
