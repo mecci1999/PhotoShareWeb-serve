d099e06da01c6f27440c7d843bbba30036d77d45
cc <664751829@qq.com>
2021-08-01 10:59:12 +0800

    定义评论相关的查询片断

---
+++ src/comment/comment.provider.ts
@@ -0,0 +1,28 @@
+/**
+ * 查询片断
+ */
+export const sqlFragment = {
+  leftJoinUser: `
+    LEFT JOIN user
+    ON user.id = comment.userId
+    LEFT JOIN avatar
+    ON user.id = avatar.userId
+  `,
+  user: `
+    JSON_OBJECT(
+      'id', user.id,
+      'name', user.name,
+      'avatar', IF(COUNT(avatar.id),1,NULL)
+    ) AS user
+  `,
+  leftJoinPosr: `
+    LEFT JOIN post
+    ON post.id = comment.postId
+  `,
+  post: `
+    JSON_OBJECT(
+      'id', post.id,
+      'title', post.title
+    ) AS post
+  `,
+};
