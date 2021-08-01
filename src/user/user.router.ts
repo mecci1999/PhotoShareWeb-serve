bb57b131a3619ed58f63c295992fb592f20c7179
cc <664751829@qq.com>
2021-08-01 18:45:45 +0800

    定义更新用户数据接口

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+import { authGuard } from '../auth/auth.middleware';
 import * as userController from './user.controller'; 
-import { validataUserData, hashPassword } from './user.middleware';
+import { validataUserData, hashPassword, validateUpdateUserData } from './user.middleware';
 
 const router = express.Router();
 
@@ -8,6 +9,11 @@ const router = express.Router();
  * 创建用户
  */
 router.post('/users', validataUserData, hashPassword, userController.store);
+
+/**
+ * 更新用户信息
+ */
+router.patch('/users', authGuard, validateUpdateUserData, userController.update);
 
 /**
  * 用户账户
