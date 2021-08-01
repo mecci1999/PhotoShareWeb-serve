2d72142a0b293320854959660e605ebabb2b5314
cc <664751829@qq.com>
2021-08-01 16:45:24 +0800

    定义获取用户账户信息的功能

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -25,26 +25,42 @@ interface GetUserOptions {
   password?: boolean;
 }
 
-export const getuserName = async (
-  name: string,
-  options: GetUserOptions = {}
-  ) => {
-  //准备选项
-  const {password} = options;
-  
-  //准备查询
-  const statement = `
-    SELECT 
-    id,
-    name
-    ${password? ',password':''}
-    FROM user
-    WHERE name = ?
-  `;
-  
-  //执行查询
-  const [...data] = await connection.promise().query(statement, name);
-  
-  //提供数据
-  return data[0][0];
+export const getUser = (condition: string) => {
+  return async (param: string | number, options: GetUserOptions = {}) => {
+    //准备选项
+    const {password} = options;
+    
+    //准备查询
+    const statement = `
+      SELECT 
+      user.id,
+      user.name,
+      IF (
+        COUNT(avatar.id), 1, NULL
+      ) AS avatar
+      ${password? ',password':''}
+      FROM
+        user
+      LEFT JOIN avatar
+        ON avatar.userId = user.id
+      WHERE
+        ${condition} = ?
+    `;
+    
+    //执行查询
+    const [...data] = await connection.promise().query(statement, param);
+    
+    //提供数据
+    return data[0][0].id ? data[0][0] : null;
+  };
 };
+
+/**
+ * 按用户名查找用户
+ */
+export const getUserByName = getUser('user.name');
+
+/**
+ * 按用户 ID 查找用户
+ */
+ export const getUserById = getUser('user.id');
