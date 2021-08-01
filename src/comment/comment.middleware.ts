f500efd32e6d9230077f873d60f78179987a0a8c
cc <664751829@qq.com>
2021-08-01 12:03:18 +0800

    获取内容的评论列表

--- src/comment/comment.middleware.ts
+++ src/comment/comment.middleware.ts
@@ -8,10 +8,22 @@ export const filter = async (
   response: Response,
   next: NextFunction
 ) => {
+  // 解构查询
+  const {post, user, action} = request.query;
+
   // 默认的过滤
   request.filter = {
     name: 'default',
     sql: 'comment.parentId IS NULL',
+  }
+
+  // 内容的评论
+  if (post && !user && !action){
+    request.filter = {
+      name: 'postComments',
+      sql: 'comment.parentId IS NULL AND postId = ?',
+      param: `${post}`,
+    }
   }
 
   //下一步
