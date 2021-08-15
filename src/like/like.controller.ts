44e10daacaba8066056e625a377ab586251e5192
cc <664751829@qq.com>
2021-08-15 17:14:01 +0800

    实时触发点赞事件

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,23 +1,33 @@
 import { Request, Response, NextFunction } from 'express';
+import { socketIoServer } from '../app/app.server';
 import { createUserLikePost, deleteUserLikePost } from './like.service';
 
 /**
-* 点赞内容
-*/
+ * 点赞内容
+ */
 export const storeUserLikePosr = async (
   request: Request,
   response: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
-  //获得数据
-  const {postId} = request.params;
-  const {id: userId} = request.user;
+  // 获得数据
+  const { postId } = request.params;
+  const { id: userId } = request.user;
 
   try {
-    //保存点赞
-    const data = await createUserLikePost(parseInt(`${userId}`, 10), parseInt(postId, 10));
+    // 保存点赞
+    const data = await createUserLikePost(
+      parseInt(`${userId}`, 10),
+      parseInt(postId, 10),
+    );
 
-    //做出响应
+    // 触发事件
+    socketIoServer.emit('userLikePostCreated', {
+      postId: parseInt(postId, 10),
+      userId,
+    });
+
+    // 做出响应
     response.status(201).send(data);
   } catch (error) {
     next(error);
@@ -25,20 +35,23 @@ export const storeUserLikePosr = async (
 };
 
 /**
-* 取消用户点赞
-*/
+ * 取消用户点赞
+ */
 export const destroyUserLikePost = async (
   request: Request,
   response: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   //获得数据
-  const {postId} = request.params;
-  const {id: userId} = request.user;
+  const { postId } = request.params;
+  const { id: userId } = request.user;
 
   try {
     //保存点赞
-    const data = await deleteUserLikePost(parseInt(`${userId}`, 10), parseInt(postId, 10));
+    const data = await deleteUserLikePost(
+      parseInt(`${userId}`, 10),
+      parseInt(postId, 10),
+    );
 
     //做出响应
     response.send(data);
