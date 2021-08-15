380abc43f57fbef658deada21766c97ff7e59185
cc <664751829@qq.com>
2021-08-15 19:11:31 +0800

    实时触发更新评论与删除评论事件

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -113,6 +113,7 @@ export const update = async (
   //准备数据
   const { commentId } = request.params;
   const { content } = request.body;
+  const socketId = request.header('X-Socket-Id');
 
   const comment = {
     id: parseInt(commentId, 10),
@@ -122,6 +123,21 @@ export const update = async (
   //修改评论
   try {
     const data = await updateComment(comment);
+
+    // 准备资源
+    const isReply = await isReplyComment(parseInt(commentId, 10));
+    const resourceType = isReply ? 'reply' : 'comment';
+    const resource = await getCommentById(parseInt(commentId, 10), {
+      resourceType,
+    });
+
+    // 触发事件
+    const eventName = isReply ? 'commentReplyUpdated' : 'commentUpdated';
+
+    socketIoServer.emit(eventName, {
+      [resourceType]: resource,
+      socketId,
+    });
 
     //做出响应
     response.send(data);
@@ -140,10 +156,26 @@ export const destroy = async (
 ) => {
   //准备数据
   const { commentId } = request.params;
+  const socketId = request.header('X-Socket-Id');
 
   //执行删除
   try {
     const data = await deleteComment(parseInt(commentId, 10));
+
+    // 准备资源
+    const isReply = await isReplyComment(parseInt(commentId, 10));
+    const resourceType = isReply ? 'reply' : 'comment';
+    const resource = await getCommentById(parseInt(commentId, 10), {
+      resourceType,
+    });
+
+    // 触发事件
+    const eventName = isReply ? 'commentReplyDeleted' : 'commentDeleted';
+
+    socketIoServer.emit(eventName, {
+      [resourceType]: resource,
+      socketId,
+    });
 
     //做出响应
     response.send(data);
