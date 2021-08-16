42a1d62f69882352897684b8988dd9f8cdc3276e
cc <664751829@qq.com>
2021-08-16 15:40:43 +0800

    定义搜索用户的服务

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -43,3 +43,52 @@ export const searchTags = async (options: searchTagsOptions) => {
   // 提供数据
   return data as any;
 };
+
+/**
+ * 搜索用户
+ */
+export interface searchUsersOptions {
+  userName: string;
+}
+
+export const searchUsers = async (options: searchUsersOptions) => {
+  // 解构数据
+  const { userName: name } = options;
+
+  // SQL 参数
+  const params: Array<any> = [`%${name}%`];
+
+  // 准备查询
+  const statement = `
+    SELECT
+      user.id,
+      user.name,
+      IF(
+        COUNT(avatar.id), 1, NULL
+      ) AS avatar,
+      (
+        SELECT
+          COUNT(post.id)
+        FROM
+          post
+        WHERE
+          user.id = post.userId
+      ) AS totalPosts
+    FROM
+      user
+    LEFT JOIN avatar
+      ON avatar.userId = user.id
+    WHERE
+      user.name LIKE ?
+    GROUP BY
+      user.id
+    LIMIT
+      10
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, params);
+
+  // 提供数据
+  return data as any;
+};
