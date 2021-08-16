5d3e3d58d39cd064abb5f1199baa11e396d75063
cc <664751829@qq.com>
2021-08-16 11:13:55 +0800

    创建搜索模块

--- src/app/index.ts
+++ src/app/index.ts
@@ -9,6 +9,7 @@ import commentRouter from '../comment/comment.router';
 import avatarRouter from '../avatar/avatar.router';
 import likeRouter from '../like/like.router';
 import appRouter from './app.router';
+import searchRouter from '../search/search.router';
 import { defaultErrorHandler } from './app.middleware';
 import { currentUser } from '../auth/auth.middleware';
 import { ALLOW_ORIGIN } from './app.config';
@@ -51,6 +52,7 @@ app.use(
   avatarRouter,
   likeRouter,
   appRouter,
+  searchRouter,
 );
 
 /**
---
+++ src/search/search.router.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import * as searchController from './search.controller';
+
+/**
+ * 定义路由
+ */
+const router = express.Router();
+
+/**
+ * 默认导出
+ */
+export default router;
