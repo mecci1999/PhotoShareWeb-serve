4920c22669b32cc019545d492362564dc4b0182e
cc <664751829@qq.com>
2021-08-18 18:18:59 +0800

    记录标签相关接口的使用情况

--- src/tag/tag.router.ts
+++ src/tag/tag.router.ts
@@ -2,13 +2,23 @@ import express from 'express';
 import * as tagMiddleware from './tag.middleware';
 import * as tagController from './tag.controller';
 import { authGuard } from '../auth/auth.middleware';
+import { accessLog } from '../access-log/access-log.middleware';
 
 const router = express.Router();
 
 /**
  * 创建标签
  */
-router.post('/tags', authGuard, tagController.store)
+router.post(
+  '/tags',
+  authGuard,
+  accessLog({
+    action: 'createTag',
+    resourceType: 'tag',
+    payloadParam: 'body.name',
+  }),
+  tagController.store,
+);
 
 /**
  * 导出默认路由
