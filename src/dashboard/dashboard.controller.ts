8ce767ddb87a00947999dee5538282d2f35949a7
cc <664751829@qq.com>
2021-08-18 23:12:08 +0800

    定义获取访问次数列表接口

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,1 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
+import { getAccessCounts, GetAccessCountsOptions } from './dashboard.service';
+
+/**
+ * 访问次数列表
+ */
+export const accessCountIndex = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { filter } = request;
+
+  try {
+    const accessCounts = await getAccessCounts({
+      filter,
+    } as GetAccessCountsOptions);
+
+    // 做出响应
+    response.send(accessCounts);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/dashboard/dashboard.router.ts
+++ src/dashboard/dashboard.router.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import * as dashBoardController from './dashboard.controller';
+import { accessCountFilter } from './dashboard.middleware';
 
 /**
  * 定义路由
  */
 const router = express.Router();
+
+/**
+ * 访问次数列表
+ */
+router.get(
+  '/dashboard/access-counts',
+  accessCountFilter,
+  dashBoardController.accessCountIndex,
+);
 
 /**
  * 默认导出
