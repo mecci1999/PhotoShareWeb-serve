8e30877906db2b3aed277965bac7e62623abb96e
cc <664751829@qq.com>
2021-08-18 18:05:45 +0800

    记录内容点赞接口相关的使用情况

--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -2,18 +2,37 @@ import express from 'express';
 import * as likeMiddleware from './like.middleware';
 import * as likeController from './like.controller';
 import { authGuard } from '../auth/auth.middleware';
+import { accessLog } from '../access-log/access-log.middleware';
 
 const router = express.Router();
 
 /**
  * 用户点赞内容
  */
-router.post('/posts/:postId/like', authGuard, likeController.storeUserLikePosr);
+router.post(
+  '/posts/:postId/like',
+  authGuard,
+  accessLog({
+    action: 'createUserLikePost',
+    resourceType: 'post',
+    resourceParamName: 'postId',
+  }),
+  likeController.storeUserLikePosr,
+);
 
 /**
  * 取消用户点赞
  */
-router.delete('/posts/:postId/like', authGuard, likeController.destroyUserLikePost);
+router.delete(
+  '/posts/:postId/like',
+  authGuard,
+  accessLog({
+    action: 'deleteUserLikePost',
+    resourceType: 'post',
+    resourceParamName: 'postId',
+  }),
+  likeController.destroyUserLikePost,
+);
 
 /**
  * 导出默认接口
