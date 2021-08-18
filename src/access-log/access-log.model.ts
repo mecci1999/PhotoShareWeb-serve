621bff898fbddee4016882a03c514166a20f096a
cc <664751829@qq.com>
2021-08-18 10:58:27 +0800

    定义访问日志数据模型

---
+++ src/access-log/access-log.model.ts
@@ -0,0 +1,19 @@
+export interface AccessLogModel {
+  id?: number;
+  userId?: number;
+  userName?: string;
+  action?: string;
+  resourceType?: string;
+  resourceId?: number;
+  payload?: string;
+  ip?: string;
+  origin?: string | string[];
+  referer?: string;
+  agent?: string;
+  language?: string;
+  originalUrl?: string;
+  method?: string;
+  query?: Object;
+  params?: Object;
+  created?: number;
+}
