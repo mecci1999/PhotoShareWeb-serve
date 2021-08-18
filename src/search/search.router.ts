869bdbae6836d18750ebec78247acdef86e021cc
cc <664751829@qq.com>
2021-08-18 18:17:00 +0800

    记录搜索相关接口的使用情况

--- src/search/search.router.ts
+++ src/search/search.router.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { accessLog } from '../access-log/access-log.middleware';
 import * as searchController from './search.controller';
 
 /**
@@ -9,12 +10,28 @@ const router = express.Router();
 /**
  * 搜索标签
  */
-router.get('/search/tags', searchController.tags);
+router.get(
+  '/search/tags',
+  accessLog({
+    action: 'searchTags',
+    resourceType: 'search',
+    payloadParam: 'query.name',
+  }),
+  searchController.tags,
+);
 
 /**
  * 搜索用户
  */
-router.get('/search/users', searchController.users);
+router.get(
+  '/search/users',
+  accessLog({
+    action: 'searchUsers',
+    resourceType: 'search',
+    payloadParam: 'query.name',
+  }),
+  searchController.users,
+);
 
 /**
  * 默认导出
