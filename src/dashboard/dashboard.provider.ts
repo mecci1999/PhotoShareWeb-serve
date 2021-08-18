d4f9ddad884dec4a66c60e8e590ded22e6f081a1
cc <664751829@qq.com>
2021-08-18 20:20:02 +0800

    定义被允许访问次数统计

---
+++ src/dashboard/dashboard.provider.ts
@@ -0,0 +1,40 @@
+export interface AccessCountListItem {
+  action: string;
+  title?: string;
+  value?: number;
+}
+
+export const allowedAccessCounts: Array<AccessCountListItem> = [
+  {
+    action: 'getPosts',
+    title: '列表访问',
+  },
+  {
+    action: 'getPostById',
+    title: '内容访问',
+  },
+  {
+    action: 'createPost',
+    title: '新增内容',
+  },
+  {
+    action: 'createComment',
+    title: '新增评论',
+  },
+  {
+    action: 'createUser',
+    title: '新增用户',
+  },
+  {
+    action: 'createUserLikePost',
+    title: '点赞内容',
+  },
+  {
+    action: 'searchTags',
+    title: '搜索标签',
+  },
+  {
+    action: 'searchUser',
+    title: '搜索用户',
+  },
+];
