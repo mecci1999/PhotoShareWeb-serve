576e8800578c9e4238c0b326ceaba3228cbe7177
cc <664751829@qq.com>
2021-08-19 21:40:05 +0800

    测试用户 Meta相关的方法

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,13 +1,21 @@
 import express, { request, response } from 'express';
+import { UserMetaType } from '../user_meta/user-mate.model';
+import {
+  createUserMeta,
+  getUserMetaByWeixinUnionId,
+  updateUserMeta,
+} from '../user_meta/user-meta.service';
 
 const router = express.Router();
 
 router.get('/', (request, response) => {
-  response.send({title: '小白兔的开发之路'});
+  response.send({ title: '小白兔的开发之路' });
 });
 
-router.post('/echo', (request,response) => {
-  response.status(201).send(request.body);
+router.post('/echo', async (request, response) => {
+  const userMeta = await getUserMetaByWeixinUnionId('321');
+
+  response.status(201).send(userMeta);
 });
 /**
  * 导出默认接口
