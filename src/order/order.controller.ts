7a2b2656fe5ed5ea4d7fcec25bceafaca5b06256
cc <664751829@qq.com>
2021-08-20 23:55:04 +0800

    定义创建订单接口

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,1 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
+import { createOrder } from './order.service';
+
+/**
+ * 创建订单
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const {
+    body: { order },
+  } = request;
+
+  try {
+    // 创建订单
+    const data = await createOrder(order);
+
+    // 作出响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/order/order.router.ts
+++ src/order/order.router.ts
@@ -1,10 +1,24 @@
 import express from 'express';
+import { accessLog } from '../access-log/access-log.middleware';
+import { authGuard } from '../auth/auth.middleware';
 import * as orderController from './order.controller';
+import { orderGuard } from './order.middleware';
 
 /**
  * 定义路由
  */
 const router = express.Router();
+
+/**
+ * 创建订单
+ */
+router.post(
+  '/orders',
+  authGuard,
+  orderGuard,
+  accessLog({ action: 'createOrder', resourceType: 'order' }),
+  orderController.store,
+);
 
 /**
  * 默认导出
