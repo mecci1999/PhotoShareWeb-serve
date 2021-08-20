32d7ae217acd32842cc4ab2df27b42bf69924da8
cc <664751829@qq.com>
2021-08-20 11:40:31 +0800

    创建支付方法数据模型

---
+++ src/payment/payment.model.ts
@@ -0,0 +1,26 @@
+export enum PaymentName {
+  wxpay = 'wxpay',
+  alipay = 'alipay',
+}
+
+export enum PaymentStatus {
+  published = 'published',
+  draft = 'draft',
+  archived = 'archived',
+}
+
+export interface PaymentMeta {
+  color?: string;
+  buttonText?: string;
+  canOffsite?: boolean;
+}
+
+export class paymentModel {
+  id?: number;
+  name?: PaymentName;
+  title?: string;
+  description?: string;
+  index?: number;
+  meta?: PaymentMeta;
+  status?: PaymentStatus;
+}
