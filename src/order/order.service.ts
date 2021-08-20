809feec22435ff1bad2eb9a559c1ea238f646ce8
cc <664751829@qq.com>
2021-08-20 23:42:34 +0800

    定义创建订单方法

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,1 +1,19 @@
 import { connection } from '../app/database/mysql';
+import { OrderModel } from './order.model';
+
+/**
+ * 创建订单
+ */
+export const createOrder = async (order: OrderModel) => {
+  // 准备查询
+  const statement = `
+    INSERT INTO \`order\`
+    SET ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, order);
+
+  // 提供数据
+  return data as any;
+};
