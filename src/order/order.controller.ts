606421901f67241e4824db73a3984b5256b43786
cc <664751829@qq.com>
2021-08-21 17:14:00 +0800

    创建与更新订单时记录订单日志

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { OrderLogAciton } from '../order-log/order-log.model';
+import { createOrderLog } from '../order-log/order-log.service';
 import { createOrder, updateOrder } from './order.service';
 
 /**
@@ -11,12 +13,25 @@ export const store = async (
 ) => {
   // 准备数据
   const {
-    body: { order },
+    body: { order, resourceType, resourceId },
+    user,
   } = request;
 
   try {
     // 创建订单
     const data = await createOrder(order);
+
+    // 创建订单日志
+    await createOrderLog({
+      userId: user.id,
+      orderId: data.insertId,
+      action: OrderLogAciton.orderCreated,
+      meta: JSON.stringify({
+        ...order,
+        resourceType,
+        resourceId,
+      }),
+    });
 
     // 作出响应
     response.status(201).send(data);
@@ -35,12 +50,23 @@ export const update = async (
 ) => {
   // 准备数据
   const {
+    user,
     body: { dataForUpdate, order },
   } = request;
 
   try {
     // 更新订单
     const data = await updateOrder(order.id, dataForUpdate);
+
+    // 创建订单日志
+    await createOrderLog({
+      userId: user.id,
+      orderId: order.id,
+      action: OrderLogAciton.orderUpdated,
+      meta: JSON.stringify({
+        ...dataForUpdate,
+      }),
+    });
 
     // 做出响应
     response.send(data);
