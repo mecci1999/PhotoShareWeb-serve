4a917284dd2bb3e7e1bc77c148e7cb20940cb615
cc <664751829@qq.com>
2021-08-21 17:03:53 +0800

    定义创建订单日志方法

---
+++ src/order-log/order-log.service.ts
@@ -0,0 +1,19 @@
+import { connection } from '../app/database/mysql';
+import { OrderLogModel } from './order-log.model';
+
+/**
+ * 创建订单日志
+ */
+export const createOrderLog = async (orderLog: OrderLogModel) => {
+  // 准备查询
+  const statement = `
+    INSERT INTO order_log
+    SET ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, orderLog);
+
+  // 提供数据
+  return data as any;
+};
