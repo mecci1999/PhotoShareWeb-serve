a991f0abaca9721461657b147f7f6f30752d104e
cc <664751829@qq.com>
2021-08-21 16:05:32 +0800

    定义更新订单守卫中间件

--- src/order/order.middleware.ts
+++ src/order/order.middleware.ts
@@ -4,6 +4,7 @@ import { getPostById, PostStatus } from '../post/post.service';
 import { ProductStatus } from '../product/product.model';
 import { getProductById } from '../product/product.service';
 import { OrderStatus } from './order.model';
+import { getOrderById } from './order.service';
 
 /**
  * 订单守卫
@@ -71,6 +72,54 @@ export const orderGuard = async (
       ...request.body,
       order,
       product,
+    };
+  } catch (error) {
+    return next(error);
+  }
+
+  // 下一步
+  next();
+};
+
+/**
+ * 更新订单守卫
+ */
+export const updateOrderGuard = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 解构数据
+  const {
+    params: { orderId },
+    body: { payment },
+  } = request;
+
+  try {
+    // 检查支付方法
+    const isValidPayment = payment && payment in PaymentName;
+
+    if (!isValidPayment) {
+      throw new Error('BAD_REQUEST');
+    }
+
+    // 检查订单
+    const order = await getOrderById(parseInt(orderId, 10));
+    const isValidOrder =
+      order &&
+      order.status === OrderStatus.pending &&
+      order.payment !== payment;
+
+    if (!isValidOrder) {
+      throw new Error('BAD_REQUEST');
+    }
+
+    // 准备主体数据
+    request.body = {
+      dataForUpdate: {
+        payment,
+      },
+      order,
     };
   } catch (error) {
     return next(error);
