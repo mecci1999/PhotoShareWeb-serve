1251292287b70503bfa6a1746a7b73b93978dc4d
cc <664751829@qq.com>
2021-08-21 19:30:54 +0800

    定义更新许可的方法

--- src/license/license.service.ts
+++ src/license/license.service.ts
@@ -17,3 +17,26 @@ export const createLicense = async (license: LicenseModel) => {
   // 提供数据
   return data as any;
 };
+
+/**
+ * 更新许可
+ */
+export const updateLicense = async (
+  licenseId: number,
+  license: LicenseModel,
+) => {
+  // 准备查询
+  const statement = `
+    UPDATE license
+    SET ?
+    WHERE license.id = ?
+  `;
+
+  // 执行查询
+  const [data] = await connection
+    .promise()
+    .query(statement, [license, licenseId]);
+
+  // 提供数据
+  return data as any;
+};
