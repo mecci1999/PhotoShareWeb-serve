9967b2d18aa5b6eaf5a3beb6fc46e1fd77d900d1
cc <664751829@qq.com>
2021-08-22 11:48:48 +0800

    创建订阅数据模型

---
+++ src/subscription/subscription.model.ts
@@ -0,0 +1,21 @@
+export enum SubscriptionType {
+  standard = 'standard',
+  pro = 'pro',
+}
+
+export enum SubscriptionStatus {
+  pending = 'pending',
+  valid = 'valid',
+  invalid = 'invalid',
+  expired = 'expired',
+}
+
+export class SubscriptionModel {
+  id?: number;
+  userId?: number;
+  orderId?: number;
+  type?: SubscriptionType;
+  status?: SubscriptionStatus;
+  created?: string;
+  expired?: string;
+}
