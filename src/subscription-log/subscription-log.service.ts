c070f316e7e91b0b99c1058faee99658ef08af06
cc <664751829@qq.com>
2021-08-22 21:44:10 +0800

    定义按订单 ID 查找订阅日志

--- src/subscription-log/subscription-log.service.ts
+++ src/subscription-log/subscription-log.service.ts
@@ -19,3 +19,27 @@ export const createSubscriptionLog = async (
   // 提供数据
   return data as any;
 };
+
+/**
+ * 按订单 ID 调取订阅日志
+ */
+export const getSubscriptionLogByOrderId = async (orderId: number) => {
+  // 准备查询
+  const statement = `
+    SELECT
+      *
+    FROM
+      subscription_log
+    WHERE
+      subscription_log.orderId = ?
+    ORDER BY
+      subscription_log.id DESC
+    LIMIT 1
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, orderId);
+
+  // 提供数据
+  return data[0] as SubscriptionLogModel;
+};
